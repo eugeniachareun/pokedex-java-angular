9b0d086099ed3b0eed037ca8bf3544daffc5665b
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-23 18:12:40 -0300

    Mapeo de entidades

---
+++ pokedex-backend-java/src/main/java/domain/Pokemon.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author euge
+ */
+@Entity
+@NamedQueries({
+  @NamedQuery(name = "Pokemon.findAll", query = "SELECT p FROM Pokemon p"),
+  @NamedQuery(name = "Pokemon.findByIdPkmn", query = "SELECT p FROM Pokemon p WHERE p.idPkmn = :idPkmn"),
+  @NamedQuery(name = "Pokemon.findByNroPkmn", query = "SELECT p FROM Pokemon p WHERE p.nroPkmn = :nroPkmn"),
+  @NamedQuery(name = "Pokemon.findByNombre", query = "SELECT p FROM Pokemon p WHERE p.nombre = :nombre"),
+  @NamedQuery(name = "Pokemon.findByDescripcion", query = "SELECT p FROM Pokemon p WHERE p.descripcion = :descripcion"),
+  @NamedQuery(name = "Pokemon.findByMostrar", query = "SELECT p FROM Pokemon p WHERE p.mostrar = :mostrar")})
+public class Pokemon implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Basic(optional = false)
+  @Column(name = "id_pkmn")
+  private Integer idPkmn;
+  @Basic(optional = false)
+  @NotNull
+  @Column(name = "nro_pkmn")
+  private int nroPkmn;
+  @Basic(optional = false)
+  @NotNull
+  @Size(min = 1, max = 45)
+  private String nombre;
+  @Size(max = 255)
+  private String descripcion;
+  private Short mostrar;
+  @JoinColumns({
+    @JoinColumn(name = "tipo1", referencedColumnName = "id_tipo"),
+    @JoinColumn(name = "tipo1", referencedColumnName = "id_tipo")})
+  @ManyToOne
+  private Tipo tipo;
+  @JoinColumns({
+    @JoinColumn(name = "tipo2", referencedColumnName = "id_tipo"),
+    @JoinColumn(name = "tipo2", referencedColumnName = "id_tipo")})
+  @ManyToOne
+  private Tipo tipo1;
+
+  public Pokemon() {
+  }
+
+  public Pokemon(Integer idPkmn) {
+    this.idPkmn = idPkmn;
+  }
+
+  public Pokemon(Integer idPkmn, int nroPkmn, String nombre) {
+    this.idPkmn = idPkmn;
+    this.nroPkmn = nroPkmn;
+    this.nombre = nombre;
+  }
+
+  public Integer getIdPkmn() {
+    return idPkmn;
+  }
+
+  public void setIdPkmn(Integer idPkmn) {
+    this.idPkmn = idPkmn;
+  }
+
+  public int getNroPkmn() {
+    return nroPkmn;
+  }
+
+  public void setNroPkmn(int nroPkmn) {
+    this.nroPkmn = nroPkmn;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getDescripcion() {
+    return descripcion;
+  }
+
+  public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+  }
+
+  public Short getMostrar() {
+    return mostrar;
+  }
+
+  public void setMostrar(Short mostrar) {
+    this.mostrar = mostrar;
+  }
+
+  public Tipo getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(Tipo tipo) {
+    this.tipo = tipo;
+  }
+
+  public Tipo getTipo1() {
+    return tipo1;
+  }
+
+  public void setTipo1(Tipo tipo1) {
+    this.tipo1 = tipo1;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (idPkmn != null ? idPkmn.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof Pokemon)) {
+      return false;
+    }
+    Pokemon other = (Pokemon) object;
+    if ((this.idPkmn == null && other.idPkmn != null) || (this.idPkmn != null && !this.idPkmn.equals(other.idPkmn))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "domain.Pokemon[ idPkmn=" + idPkmn + " ]";
+  }
+  
+}
---
+++ pokedex-backend-java/src/main/java/domain/Usuario.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author euge
+ */
+@Entity
+@NamedQueries({
+  @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u"),
+  @NamedQuery(name = "Usuario.findByIdUsuario", query = "SELECT u FROM Usuario u WHERE u.idUsuario = :idUsuario"),
+  @NamedQuery(name = "Usuario.findByUsername", query = "SELECT u FROM Usuario u WHERE u.username = :username"),
+  @NamedQuery(name = "Usuario.findByPassword", query = "SELECT u FROM Usuario u WHERE u.password = :password"),
+  @NamedQuery(name = "Usuario.findByPrivileges", query = "SELECT u FROM Usuario u WHERE u.privileges = :privileges")})
+public class Usuario implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Basic(optional = false)
+  @Column(name = "id_usuario")
+  private Integer idUsuario;
+  @Size(max = 16)
+  private String username;
+  @Size(max = 16)
+  private String password;
+  private Short privileges;
+
+  public Usuario() {
+  }
+
+  public Usuario(Integer idUsuario) {
+    this.idUsuario = idUsuario;
+  }
+
+  public Integer getIdUsuario() {
+    return idUsuario;
+  }
+
+  public void setIdUsuario(Integer idUsuario) {
+    this.idUsuario = idUsuario;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Short getPrivileges() {
+    return privileges;
+  }
+
+  public void setPrivileges(Short privileges) {
+    this.privileges = privileges;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (idUsuario != null ? idUsuario.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof Usuario)) {
+      return false;
+    }
+    Usuario other = (Usuario) object;
+    if ((this.idUsuario == null && other.idUsuario != null) || (this.idUsuario != null && !this.idUsuario.equals(other.idUsuario))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "domain.Usuario[ idUsuario=" + idUsuario + " ]";
+  }
+  
+}
