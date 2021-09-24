7759dc150802d49f3dd6e838853282d349f6f806
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-24 10:21:13 -0300

    Interface de datos PokemonDao

---
+++ pokedex-backend-java/src/main/java/data/PokemonDao.java
@@ -0,0 +1,21 @@
+package data;
+
+import domain.Pokemon;
+import domain.Tipo;
+import java.util.List;
+
+/**
+ *
+ * @author Eugenia Chareun
+ */
+public interface PokemonDao {
+  public List<Pokemon> getAllPokemons();
+  public List<Pokemon> getAllPokemonsByTipo(Tipo tipo);
+  public Pokemon getPokemonById(Integer idPkmn);
+  public Pokemon getPokemonByNro(Integer nroPkmn);
+  public Pokemon getPokemonByNombre(String nombre);
+  public void insertPokemon(Pokemon pkmn);
+  public void updatePokemon(Pokemon pkmn);
+  public void deletePokemon(Pokemon pkmn);
+  public void hidePokemon(Pokemon pkmn);
+}
