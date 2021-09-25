ed42e74e5eba0bdf7cebb0c51ccb98e56d2e10f6
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-25 14:38:08 -0300

    Activador JAXRS

---
+++ pokedex-backend-java/src/main/java/web/JAXRSActivator.java
@@ -0,0 +1,13 @@
+package web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Eugenia Chareun <eugenia.chareun@gmail.com>
+ */
+@ApplicationPath("webservice")
+public class JAXRSActivator extends Application{
+  
+}
