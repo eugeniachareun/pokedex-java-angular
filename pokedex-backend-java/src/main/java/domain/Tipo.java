d858cb6470afc497340e119da6ab5dd0f4ae3e81
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-25 18:33:20 -0300

    RESTful webservice listo

--- pokedex-backend-java/src/main/java/domain/Tipo.java
+++ pokedex-backend-java/src/main/java/domain/Tipo.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domain;
 
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
