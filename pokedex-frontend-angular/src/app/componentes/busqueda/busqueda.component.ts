d7a509c98c8be097e2beaf0c9e85389404b0fe5f
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-27 14:53:36 -0300

    Componente de búsqueda

---
+++ pokedex-frontend-angular/src/app/componentes/busqueda/busqueda.component.ts
@@ -0,0 +1,30 @@
+import { PokemonService } from './../../service/pokemon.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent implements OnInit {
+
+  busquedaInput! : string;
+
+  constructor(private pkmnService : PokemonService) { }
+
+  ngOnInit(): void {
+  }
+
+  onBuscar(){
+    this.busquedaInput = this.busquedaInput.trim();
+    const regex = /^[0-9]*$/;
+
+    if(regex.test(this.busquedaInput)){
+      const nroPkmn = parseInt(this.busquedaInput);
+      this.pkmnService.obtenerPokemonPorNro(nroPkmn);
+    } else {
+      this.pkmnService.obtenerPokemonPorNombre(this.busquedaInput);
+    }
+  }
+}
