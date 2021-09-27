cc650075cdfed7c04fe2f1a5ee4760c73b47086e
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-27 13:55:22 -0300

    Import corregido

--- pokedex-frontend-angular/src/app/app-routing.module.ts
+++ pokedex-frontend-angular/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BusquedaComponent } from './busqueda/busqueda.component';
+import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
+
 
 const routes: Routes = [
   {path: '', component: BusquedaComponent}
