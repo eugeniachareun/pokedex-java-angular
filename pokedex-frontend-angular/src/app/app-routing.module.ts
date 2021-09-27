24044a3dd1a8cbb111c3baace0f9caf27a396c0c
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-27 18:35:52 -0300

    Correción rutas

--- pokedex-frontend-angular/src/app/app-routing.module.ts
+++ pokedex-frontend-angular/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { PokemonsComponent } from './componentes/pokemons/pokemons.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
 
 
 const routes: Routes = [
-  {path: '', component: BusquedaComponent}
+  {path: '', component: BusquedaComponent},
+  {path: 'all', component: PokemonsComponent}
 ];
 
 @NgModule({
