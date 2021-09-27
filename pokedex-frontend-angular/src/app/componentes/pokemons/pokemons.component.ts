0c85e78f728137f20eccfd8b3d865a7ef41eb01e
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-27 18:35:20 -0300

    Componente pokemons v1

--- pokedex-frontend-angular/src/app/componentes/pokemons/pokemons.component.ts
+++ pokedex-frontend-angular/src/app/componentes/pokemons/pokemons.component.ts
@@ -1,16 +1,27 @@
+import { PokemonService } from './../../service/pokemon.service';
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/model/pokemon.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pokemons',
   templateUrl: './pokemons.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PokemonsComponent implements OnInit {
+  pokemons: Pokemon[] = [];
 
-  constructor() { }
+  constructor(
+    private pkmnService: PokemonService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.pkmnService.obtenerPokemons().subscribe((pokemonsObtenidos: any) => {
+      this.pokemons = pokemonsObtenidos;
+      this.pkmnService.setPokemons(this.pokemons);
+      console.log('Pokemons recuperados de la base de datos: ' + pokemonsObtenidos);
+    });
   }
-
 }
