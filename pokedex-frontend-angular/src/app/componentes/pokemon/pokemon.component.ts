54e3bf31551b8c56223025570cf4c9800e0c7320
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-28 19:37:29 -0300

    Recupear pokemon siguiente

--- pokedex-frontend-angular/src/app/componentes/pokemon/pokemon.component.ts
+++ pokedex-frontend-angular/src/app/componentes/pokemon/pokemon.component.ts
@@ -12,19 +12,32 @@ import { Pokemon } from 'src/app/model/pokemon.model';
 export class PokemonComponent implements OnInit {
   nombre : string = '';
   pkmn! : Pokemon;
+  nextPkmn! : Pokemon;
 
 
   constructor(private pkmnService : PokemonService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    //Recupera el pokemon a partir del nombre
     this.nombre = this.route.snapshot.params['nombre'];
     this.pkmnService.recuperarPokemonPorNombre(this.nombre).subscribe(
       (pkmnRecuperado : any) => {
         this.pkmn = pkmnRecuperado;
-        console.log('Pokemon recuperado por nombre de la base de datos: ' + pkmnRecuperado);
+        console.log('Pokemon recuperado por nombre desde la base de datos: ' + pkmnRecuperado);
       }
     );
+
+    //Si el pokemon NO es el último, recupera el pokemon siguiente
+    if(this.pkmn.nroPkmn != '151'){
+      let idNextPkmn : number = this.pkmn.idPkmn! + 1;
+      this.pkmnService.recuperarPokemonPorId(idNextPkmn).subscribe(
+        (pkmnRecuperado : any) => {
+          this.nextPkmn = pkmnRecuperado;
+          console.log('Pokemon recuperado por el ID desde la base de datos: ' + pkmnRecuperado);
+        }
+      );
+    }
   }
 
 }
