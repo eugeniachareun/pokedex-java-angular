a7fe3c569805fe212a78c0edafd07476b6c2ea5d
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-29 15:09:51 -0300

    Remove imports innecesarios

--- pokedex-frontend-angular/src/app/componentes/pokemons/pokemons.component.ts
+++ pokedex-frontend-angular/src/app/componentes/pokemons/pokemons.component.ts
@@ -1,7 +1,6 @@
 import { PokemonService } from './../../service/pokemon.service';
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/model/pokemon.model';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pokemons',
@@ -11,14 +10,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class PokemonsComponent implements OnInit {
   pokemons: Pokemon[] = [];
 
-  constructor(
-    private pkmnService: PokemonService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private pkmnService: PokemonService) {}
 
   ngOnInit(): void {
-    this.pkmnService.obtenerPokemons().subscribe((pokemonsObtenidos: any) => {
+    this.pkmnService.obtenerPokemons().subscribe(
+      (pokemonsObtenidos: any) => {
       this.pokemons = pokemonsObtenidos;
       this.pkmnService.setPokemons(this.pokemons);
       console.log('Pokemons recuperados de la base de datos: ' + pokemonsObtenidos);
