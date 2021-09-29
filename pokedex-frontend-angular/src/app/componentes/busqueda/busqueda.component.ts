a706291768b05a3d1896a55f8bfb3b99d374ac6b
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-29 15:08:58 -0300

    Implementación del método onBuscar (validación)

--- pokedex-frontend-angular/src/app/componentes/busqueda/busqueda.component.ts
+++ pokedex-frontend-angular/src/app/componentes/busqueda/busqueda.component.ts
@@ -1,29 +1,47 @@
 import { PokemonService } from './../../service/pokemon.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pokemon } from 'src/app/model/pokemon.model';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BusquedaComponent implements OnInit {
+  busquedaInput!: string;
+  pkmn: Pokemon | undefined;
 
-  busquedaInput! : string;
-
-  constructor(private pkmnService : PokemonService) { }
+  constructor(private pkmnService: PokemonService, private router: Router) {}
 
   ngOnInit(): void {
   }
 
-  onBuscar(){
-    this.busquedaInput = this.busquedaInput.trim();
+  onBuscar() {
+    this.busquedaInput = this.busquedaInput!.trim();
     const regex = /^[0-9]*$/;
 
-    if(regex.test(this.busquedaInput)){
-      this.pkmnService.obtenerPokemonPorNro(this.busquedaInput);
+    //Si el string a buscar es un número, se llama a zeroFill y se ejecuta la búsqueda por número.
+    //Si es un string de letras, se busca por nombre.
+    if (regex.test(this.busquedaInput)) {
+      this.busquedaInput = this.pkmnService.zeroFill(parseInt(this.busquedaInput));
+      this.pkmnService.recuperarPokemonPorNro(this.busquedaInput).subscribe(
+        (pkmnRecuperado : any) => {
+          this.pkmn = pkmnRecuperado;
+        }
+      )
+
     } else {
-      this.pkmnService.obtenerPokemonPorNombre(this.busquedaInput);
+      this.pkmnService.recuperarPokemonPorNombre(this.busquedaInput).subscribe(
+        (pkmnRecuperado : any) => {
+          this.pkmn = pkmnRecuperado;
+        }
+      )
+    }
+
+    if (this.pkmn != null) {
+      const nombrePkmn: string = this.pkmn.nombre!;
+      this.router.navigate(['/', nombrePkmn]);
     }
   }
 }
