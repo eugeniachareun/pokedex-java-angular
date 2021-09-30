3da21fca4537bb4bd9278d003b5b10d6f76a146b
Eugenia Chareun <eugenia.chareun@gmail.com>
2021-09-30 13:07:04 -0300

    Select dinámico, con arrayTipos y ngModel mejorado

--- pokedex-frontend-angular/src/app/componentes/pokemon/pokemon.component.ts
+++ pokedex-frontend-angular/src/app/componentes/pokemon/pokemon.component.ts
@@ -1,3 +1,4 @@
+import { TipoService } from './../../service/tipo.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from 'src/app/service/pokemon.service';
@@ -17,17 +18,16 @@ export class PokemonComponent implements OnInit {
   previousPkmn! : Pokemon;
   nextPkmn! : Pokemon;
   visibilidadModal : boolean = false;
+  arrayTipos : Tipo[] = this.tipoService.getArreglo();
+
 
   //Atributos de form
-  nombrePkmnForm! :string;
-  nroPkmnForm! : string;
-  tipo1PkmnForm! : string;
-  tipo2PkmnForm! : string;
-  descripcionPkmnForm! :string
-
+  tipo1PkmnForm : string = '';
+  tipo2PkmnForm : string = '';
 
   constructor(private pkmnService : PokemonService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private tipoService : TipoService) { }
 
   ngOnInit(): void {
     //Recupera el pokemon a partir del nombre
@@ -37,18 +37,23 @@ export class PokemonComponent implements OnInit {
         this.pkmn = pkmnRecuperado;
         console.log('Pokemon recuperado por nombre desde la base de datos: ' + pkmnRecuperado);
 
+        //Si el pokemon NO es el primero, recupera el pokemon anterior
         if(this.pkmn.nroPkmn != '001'){
           this.recuperarPreviousPkmn();
         }
+        //Si el pokemon NO es el último, recupera el pokemon siguiente
         if(this.pkmn.nroPkmn! != '151'){
           this.recuperarNextPkmn();
         }
       }
     );
+
+    //Inicializa atributos de form con valores del pokemon convertidos a string
+    this.tipo1PkmnForm = this.pkmn.tipo1!.idTipo + '';
+    this.tipo2PkmnForm = this.pkmn.tipo2!.idTipo + '';
   }
 
   recuperarPreviousPkmn(){
-    //Si el pokemon NO es el primero, recupera el pokemon anterior
       const nroPreviousPkmn : number = parseInt(this.pkmn.nroPkmn!) - 1;
       const strPreviousPkmn : string = this.pkmnService.zeroFill(nroPreviousPkmn);
       this.pkmnService.recuperarPokemonPorNro(strPreviousPkmn).subscribe(
@@ -60,7 +65,6 @@ export class PokemonComponent implements OnInit {
       }
 
   recuperarNextPkmn(){
-    //Si el pokemon NO es el último, recupera el pokemon siguiente
       const nroNextPkmn : number = parseInt(this.pkmn.nroPkmn!) + 1;
       const strNextPkmn : string = this.pkmnService.zeroFill(nroNextPkmn);
       this.pkmnService.recuperarPokemonPorNro(strNextPkmn).subscribe(
@@ -80,16 +84,19 @@ export class PokemonComponent implements OnInit {
   }
 
   guardarPokemon(){
-    //Recupera los tipos
-    const tipo1 : Tipo = new Tipo(parseInt(this.tipo1PkmnForm));
-    const tipo2 : Tipo = new Tipo(parseInt(this.tipo2PkmnForm));
+    //Crea los tipos a partir de sus ID
+    const tipo1: Tipo = new Tipo(parseInt(this.tipo1PkmnForm));
+    const tipo2: Tipo = new Tipo(parseInt(this.tipo2PkmnForm));
 
     //Crea el pokemon a partir del formulario
-    const pkmnForm : Pokemon = new Pokemon(this.pkmn.idPkmn, this.nroPkmnForm, this.nombrePkmnForm,
-                                          tipo1, tipo2, this.descripcionPkmnForm);
+    const pkmnForm : Pokemon = new Pokemon(this.pkmn.idPkmn, this.pkmn.nroPkmn, this.pkmn.nombre,
+                                          tipo1, tipo2, this.pkmn.descripcion);
 
     //Llama al servicio
     this.pkmnService.modificarPokemon(this.pkmn.idPkmn!, pkmnForm);
+
+    //Oculta el modal
+    this.ocultarModal();
   }
 
 }
